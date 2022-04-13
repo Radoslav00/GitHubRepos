5c6f554404f4f4dcf9a7feb40d9098ef40b534c7
Radoslav00 <radoslavlambrev@gmail.com>
2022-04-13 16:47:54 +0300

    Search feature added

--- app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposItemFragment.kt
+++ app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposItemFragment.kt
@@ -1,7 +1,10 @@
 package com.radoslav.githubrepos.ui.repos
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.radoslav.githubrepos.R
@@ -32,6 +35,33 @@ class ReposItemFragment : Fragment(R.layout.fragment_repos) {
         viewModel.repos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+        setHasOptionsMenu(true)
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_repos, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if(query !=null){
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchRepos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
