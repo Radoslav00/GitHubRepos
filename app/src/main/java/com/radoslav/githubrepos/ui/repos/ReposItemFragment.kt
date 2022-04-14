dce5a135657ab54aa27f9e0095518376b1fcffe4
Radoslav00 <radoslavlambrev@gmail.com>
2022-04-14 16:28:47 +0300

    Proccess death handled

--- app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposItemFragment.kt
+++ app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposItemFragment.kt
@@ -1,18 +1,23 @@
 package com.radoslav.githubrepos.ui.repos
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.radoslav.githubrepos.R
+import com.radoslav.githubrepos.data.GitRepoItem
 import com.radoslav.githubrepos.databinding.FragmentReposBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ReposItemFragment : Fragment(R.layout.fragment_repos) {
+class ReposItemFragment : Fragment(R.layout.fragment_repos), GitReposAdapter.OnItemClickListener {
     private val viewModel by viewModels<ReposViewModel>()
 
     private var _binding: FragmentReposBinding? = null
@@ -23,22 +28,54 @@ class ReposItemFragment : Fragment(R.layout.fragment_repos) {
 
         _binding = FragmentReposBinding.bind(view)
 
-        val adapter = GitReposAdapter()
+        val adapter = GitReposAdapter(this)
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
+            recyclerView.itemAnimator = null
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = ItemRepoLoadStateAdapter { adapter.retry() },
                 footer = ItemRepoLoadStateAdapter { adapter.retry() }
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
         viewModel.repos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh !is LoadState.Loading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+
+                //for empty view
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1
+                ) {
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+            }
+        }
+
         setHasOptionsMenu(true)
     }
 
 
+    override fun onItemClick(repo: GitRepoItem) {
+        val uri = Uri.parse(repo.html_url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        startActivity(intent)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -48,9 +85,9 @@ class ReposItemFragment : Fragment(R.layout.fragment_repos) {
         val searchItem = menu.findItem(R.id.action_search)
         val searchView = searchItem.actionView as SearchView
 
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if(query !=null){
+                if (query != null) {
                     binding.recyclerView.scrollToPosition(0)
                     viewModel.searchRepos(query)
                     searchView.clearFocus()
--- app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposViewModel.kt
+++ app/src/main/java/com/radoslav/githubrepos/ui/repos/ReposViewModel.kt
@@ -1,20 +1,19 @@
 package com.radoslav.githubrepos.ui.repos
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.radoslav.githubrepos.data.GitRepository
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ReposViewModel @Inject constructor(
-    private val repository: GitRepository
+    private val repository: GitRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val repos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -25,6 +24,7 @@ class ReposViewModel @Inject constructor(
     }
 
     companion object{
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "hello world"
     }
 }
